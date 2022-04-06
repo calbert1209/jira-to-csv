122f529fa3a4690e7ba136a16ccf3b2ce76879dd
Christian Albert <c.albert@shippio.io>
2022-04-06 09:57:14 +0900

    add example of full JSON response

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ const { fetchIssues } = require("./fetch-issues");
 const { parseArgs } = require("./parse-args");
 const { csvLines } = require("./create-csv");
 
-if (process.argv.length <= 2) {
-  console.log("node ./path/to/script.js <team> <sprint> <api_token>");
-  return;
-}
-
-const { sprint, team, authToken } = parseArgs();
-
-fetchIssues(sprint, team, authToken).then((issues) => {
-  const output = csvLines(issues).join("\n");
-  console.log(output);
-});
+(function run() {
+  if (process.argv.length <= 2) {
+    console.log("node ./path/to/script.js <team> <sprint> <api_token>");
+    return;
+  }
+  
+  const { sprint, team, authToken } = parseArgs();
+  
+  fetchIssues(sprint, team, authToken).then((issues) => {
+    const output = csvLines(issues).join("\n");
+    console.log(output);
+  });
+})();
